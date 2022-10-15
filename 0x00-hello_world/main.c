08839c0a24f76546ddf5ff5a6fdd954930445431
mikael2012 <mikaelmesfin@gmail.com>
2022-10-15 18:34:33 +0300

    q1

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,9 +1,23 @@
-#include <stdio.h>
+#inclu#include <stdio.h>
+
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Retuern: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+int main(void)
+{
+    return (0);
+}
+julien@ubuntu:~/c/0x00$ export CFILE=main.c
+julien@ubuntu:~/c/0x00$ ./0-preprocessor 
+julien@ubuntu:~/c/0x00$ tail c
+# 942 "/usr/include/stdio.h" 3 4
+
+# 2 "main.c" 2
+
+
+# 3 "main.c"
 int main(void)
 {
  return (0);
