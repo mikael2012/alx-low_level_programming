84097aa5aa571ad7e5df367cc6bb2045eb1e7911
mikael2012 <mikaelmesfin@gmail.com>
2023-03-15 01:15:45 -0700

    rep

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,36 +1,39 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * str_concat - function that concatenates two strings.
- * @s1: 1ststring
- * @s2: 2nd string
- * Return: pointer to the new string or null if null passed
- **/
-char *str_concat(char *s1, char *s2)
-{
-	char *str;
-	int len1 = 0;
-	int len2 = 0;
-	int i, j;
+ * **alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: int
+ * @height: int
+ * Return: 2d array
+ */
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
-	len2++;
-	str = malloc(sizeof(char) * (len1 + len2));
-	if (str == NULL)
+int **alloc_grid(int width, int height)
+{
+	int **tab, i, j;
+
+	tab = malloc(sizeof(*tab) * height);
+
+	if (width <= 0 || height <= 0 || tab == 0)
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (s1[i])
-		str[j++] = s1[i++];
-	i = 0;
-	while (s2[i])
-		str[j++] = s2[i++];
-	return (str);
+
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			tab[i] = malloc(sizeof(**tab) * width);
+		if (tab[i] == 0)
+		{
+		/*Free everything if malloc fails*/
+		while (i--)
+			free(tab[i]);
+		free(tab);
+		return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			tab[i][j] = 0;
+	}
+}
+
+return (tab);
 }
